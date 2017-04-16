e12cebc75be205cae96baae6732802b13e11c487
Andrew Rush <inarush0@gmail.com>
2017-04-15 22:28:44 -0400

    Adding logging functionality

--- routes.go
+++ routes.go
@@ -19,13 +19,19 @@ type Routes []Route
 
 // NewRouter creates a new gorilla/mux router
 func NewRouter() *mux.Router {
+
 	router := mux.NewRouter().StrictSlash(true)
 	for _, route := range routes {
+		var handler http.Handler
+
+		handler = route.HandlerFunc
+		handler = Logger(handler, route.Name)
+
 		router.
 			Methods(route.Method).
 			Path(route.Pattern).
 			Name(route.Name).
-			Handler(route.HandlerFunc)
+			Handler(handler)
 	}
 
 	return router
